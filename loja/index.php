763cd8ed3a3bba5fe91dbed4239148ea0c70b2fd
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2018-02-23 22:20:47 -0300

    Aula 01 - Praticando a sintaxe
    Criando função para soma de elementos numéricos de um array

--- loja/index.php
+++ loja/index.php
@@ -2,4 +2,26 @@
 
 <h1>Hello, world!</h1>
 
+<?php 
+
+	function somarArray($array) 
+	{
+		$total = 0;
+
+		for($i = 0; $i < sizeof($array); $i++)
+		{
+			if(gettype($array[$i]) == "integer")
+				$total += $array[$i];
+		}	
+
+		return $total;
+	}
+
+	$meuArray = array(1, 2, 3, 4, 5, "banana");
+
+	echo somarArray($meuArray);
+?>
+
+
+
 <?php include("rodape.php")?>
