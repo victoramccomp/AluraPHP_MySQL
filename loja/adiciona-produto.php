62e16df3a6b1544f2b37b8e1019606ab6451880a
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2018-03-02 20:41:16 -0300

    Aula 03 - Inserindo produtos no banco de dados

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -3,8 +3,14 @@
 <?php
     $nome = $_GET["nome"];
     $preco = $_GET["preco"];
-?>
+    $conn = mysqli_connect('localhost', 'root', '', 'alura_phpmysql_loja');
 
-<p class="alert-success"> Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!!! </p>
+    $query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}', {$preco})";
+    if(mysqli_query($conn, $query)) {?>
+        <p class="alert-success"> Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!!! </p>
+    <?php } else { ?>
+        <p class="alert-danger"> Produto <?= $nome; ?>não foi adicionado!!! </p>
+    <?php } ?>
+
 
 <?php include("rodape.php")?>
